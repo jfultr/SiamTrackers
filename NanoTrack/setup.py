7e3abbfd69f34d093c6e0bf5640d15a41eb8a0cf
Farkhat Yakubov <farhatyalubov@gmail.com>
2024-08-16 12:19:27 +0300

    setup.py cython install now optional

--- NanoTrack/setup.py
+++ NanoTrack/setup.py
@@ -1,22 +1,37 @@
 from distutils.core import setup
 from distutils.extension import Extension
-from Cython.Build import cythonize
 
-ext_modules = [
-    Extension(
-        name='toolkit.utils.region',
-        sources=[
-            'toolkit/utils/region.pyx',
-            'toolkit/utils/src/region.c',
-        ],
-        include_dirs=[
-            'toolkit/utils/src'
-        ]
+
+without_cython = False
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    without_cython = True
+    print('WARNING: Package installed WITHOUT region fucntion! please install cython=0.29 to use it')
+
+
+if not without_cython:
+    ext_modules = [
+        Extension(
+            name='toolkit.utils.region',
+            sources=[
+                'toolkit/utils/region.pyx',
+                'toolkit/utils/src/region.c',
+            ],
+            include_dirs=[
+                'toolkit/utils/src'
+            ]
+        )
+    ]
+
+if not without_cython:
+    setup(
+        name='toolkit',
+        packages=['toolkit'],
+        ext_modules=cythonize(ext_modules)
     )
-]
-
-setup(
-    name='toolkit',
-    packages=['toolkit'],
-    ext_modules=cythonize(ext_modules)
-)
+else:
+    setup(
+        name='toolkit',
+        packages=['toolkit'],
+    ) 
