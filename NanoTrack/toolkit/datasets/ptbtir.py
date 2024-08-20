3ea374076a4b0b4abfbb2cd62878786f35a8615e
Farkhat Yakubov <farhatyakubov@gmail.com>
2024-08-20 12:01:20 +0000

    added load tracker -> ptbtir

--- NanoTrack/toolkit/datasets/ptbtir.py
+++ NanoTrack/toolkit/datasets/ptbtir.py
@@ -2,6 +2,7 @@ import json
 import os
 
 from tqdm import tqdm
+from PIL import Image
 
 from .dataset import Dataset
 from .video import Video
@@ -25,6 +26,39 @@ class PtbTirVideo(Video):
             gt_rect, attr, load_img=False):
         super(PtbTirVideo, self).__init__(name, root, video_dir,
                 init_rect, img_names, gt_rect, attr, load_img)
+        
+        self.gt_traj = [[0] if np.isnan(bbox[0]) else bbox
+                for bbox in self.gt_traj]  
+
+        self.confidence = {}
+    
+    def load_tracker(self, path, tracker_names=None, store=True):
+            """
+            Args:
+                path(str): path to result
+                tracker_name(list): name of tracker
+            """
+            if not tracker_names:
+                tracker_names = [x.split('/')[-1] for x in glob(path)
+                        if os.path.isdir(x)]
+            if isinstance(tracker_names, str):
+                tracker_names = [tracker_names]
+            for name in tracker_names:
+                traj_file = os.path.join(path, name, 'longterm',
+                        self.name, self.name+'_001.txt')
+                with open(traj_file, 'r') as f:
+                    traj = [list(map(float, x.strip().split(',')))
+                            for x in f.readlines()]
+                if store:
+                    self.pred_trajs[name] = traj
+                confidence_file = os.path.join(path, name, 'longterm',
+                        self.name, self.name+'_001_confidence.value')
+                with open(confidence_file, 'r') as f:
+                    score = [float(x.strip()) for x in f.readlines()[1:]]
+                    score.insert(0, float('nan'))
+                if store:
+                    self.confidence[name] = score
+            return traj, score
 
 
 class PtbTirDataset(Dataset):
